a7711dc62f9513befbaeea55c797a2f9f246a701
Co3co <xco3cox@gmail.com>
2022-07-11 18:18:38 -0400

    made it shorter

--- keypressed.c
+++ keypressed.c
@@ -2,16 +2,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int f2p(float fraction, float base) {
-    return (fraction / base) * 100;
-}
-
 int main() {
     if (access("/tmp/presslog", F_OK) != 0) {
         return 0;
     }
 
-    float percent, pressTime;
     char logLines[1048576], keyRepeatSpeed[24];
 
     FILE *flogLines = popen("wc -l /tmp/presslog | awk '{print $1}'", "r");
@@ -23,9 +18,7 @@ int main() {
     fgets(keyRepeatSpeed, 24, fkeyRepeatSpeed);
     fclose(fkeyRepeatSpeed);
 
-    percent = f2p(atoi(logLines), atoi(keyRepeatSpeed));
-    pressTime = (percent / 100) * 1000;
-    printf("%.0f", pressTime);
+    printf("%.0f", atof(logLines) / atof(keyRepeatSpeed) * 1000);
     return 0;
 }
 
