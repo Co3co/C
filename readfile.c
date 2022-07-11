3b7df018b431a4b0694977a306ddf1cea90110eb
Co3co <xco3cox@gmail.com>
2022-07-11 19:49:29 -0400

    made it like 6x faster or something

--- readfile.c
+++ readfile.c
@@ -1,43 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
-
-struct File {
-    char name[256];
-    char contents[1048576];
-    char lines[1048576];
-};
 
 int main(int argc, char **argv) {
     if (argv[1] == NULL) {
-        puts("Please supply a file to read.");
+        puts("Please provide a file to read.");
         return 0;
     }
 
-    struct File file;
-    strcpy(file.name, argv[1]);
-    char fileLine[1048576];
-    char cmd[256];
+    char fileName[256];
+    char buf;
+    strcpy(fileName, argv[1]);
 
-    if (access(file.name, F_OK) != 0) {
+    if (access(fileName, F_OK) != 0) {
         puts("File does not exist.");
         return 0;
     }
 
-    snprintf(cmd, sizeof(cmd), "wc -l %s | awk '{print $1}'", file.name);
-    FILE *fLines = popen(cmd, "r");
-    fgets(file.lines, 1024, fLines);
-    fclose(cmd);
-
-    FILE *pFile = fopen(file.name, "r");
-    for(int i = 0; i < atoi(file.lines); i++) {
-        fgets(fileLine, 255, pFile);
-        strcat(file.contents, fileLine);
+    FILE *file = fopen(fileName, "r");
+    for (buf = getc(file); buf != EOF; buf = getc(file)) {
+        printf("%c", buf);
+        fflush(stdout);
     }
-    fclose(pFile);
-
-    printf("%s", file.contents);
+    fclose(file);
     return 0;
 }
 
