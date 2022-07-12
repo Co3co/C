4ac8488738a1269a040349a5c554731a9fb8c425
Co3co <xco3cox@gmail.com>
2022-07-11 20:41:48 -0400

    option for speed or frequent updates

--- readfile.c
+++ readfile.c
@@ -10,7 +10,14 @@ int main(int argc, char **argv) {
 
     char fileName[256];
     char buf;
-    strcpy(fileName, argv[1]);
+    int acc;
+    if (strcmp(argv[1], "-acc") == 0) {
+        acc = 1;
+        strcpy(fileName, argv[2]);
+    } else {
+        acc = 0;
+        strcpy(fileName, argv[1]);
+    }
 
     if (access(fileName, F_OK) != 0) {
         puts("File does not exist.");
@@ -20,6 +27,8 @@ int main(int argc, char **argv) {
     FILE *file = fopen(fileName, "r");
     while (buf != EOF) {
         printf("%c", buf);
+        if (acc == 1)
+            fflush(stdout);
         buf = getc(file);
     }
     fclose(file);
