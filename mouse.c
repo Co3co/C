ef1748c456ee7db4d0d236345242bd66edd6926c
Co3co <xco3cox@gmail.com>
2022-07-13 22:51:27 -0400

    added keypressed.c function to mouse.c and redid some stuff

--- mouse.c
+++ mouse.c
@@ -9,15 +9,18 @@ charcodemap_t *mods;
 int nmods;
 
 void click(int button);
-void click_cords(int x, int y, int screen, int button);
-void click_cords_return(int x, int y, int screen, int button);
+void click_cords(int x, int y, int button, int screen);
+void click_cords_return(int x, int y, int button, int screen);
 void move(int x, int y, int screen);
 void move_rel(int x, int y);
+void move_smooth(float tx, float ty, int speedMS, int screen);
 void get_coords(int *x, int *y, int *screen);
-void hold(int pressTime, int button);
-void drag(float ix, float iy, float tx, float ty, int speedMS, int screen, int button);
+void get_coords_click(int *x, int *y, int *screen);
+void hold(int holdTime, int button);
+void drag(float ix, float iy, float tx, float ty, int speedMS, int button, int screen);
 void get_pixel_color(char color[]);
-int print_help(void);
+int get_hold_time(float *holdTime);
+void print_help(void);
 
 void click(int button) {
     xdo_get_active_modifiers(xdo, &mods, &nmods);
@@ -29,7 +32,7 @@ void click(int button) {
     xdo_set_active_modifiers(xdo, CURRENTWINDOW, mods, nmods);
 }
 
-void click_cords(int x, int y, int screen, int button) {
+void click_cords(int x, int y, int button, int screen) {
     xdo_get_active_modifiers(xdo, &mods, &nmods);
     xdo_clear_active_modifiers(xdo, CURRENTWINDOW, mods, nmods);
 
@@ -40,7 +43,7 @@ void click_cords(int x, int y, int screen, int button) {
     xdo_set_active_modifiers(xdo, CURRENTWINDOW, mods, nmods);
 }
 
-void click_cords_return(int x, int y, int screen, int button) {
+void click_cords_return(int x, int y, int button, int screen) {
     xdo_get_active_modifiers(xdo, &mods, &nmods);
     xdo_clear_active_modifiers(xdo, CURRENTWINDOW, mods, nmods);
 
@@ -63,17 +66,48 @@ void move_rel(int x, int y) {
     xdo_move_mouse_relative(xdo, x, y);
 }
 
+void move_smooth(float tx, float ty, int speedMS, int screen) {
+    float frac, loop, loops, x, y, posdx, posdy;
+    int ix, iy;
+    xdo_get_mouse_location(xdo, &ix, &iy, &screen);
+    float dx = tx - ix, dy = ty - iy;
+    if (dx < 0 || dy < 0) {
+        posdx = (dx < 0) ? dx - dx * 2 : dx;
+        posdy = (dy < 0) ? dy - dy * 2 : dy;
+        loops = (posdx < posdy) ? posdy : posdx;
+    } else {
+        loops = (dx < dy) ? dy : dx;
+    }
+
+    for (loop = 1; loop <= loops; loop++) {
+        frac = loop / loops;
+        x = ix + (frac * dx);
+        y = iy + (frac * dy);
+        xdo_move_mouse(xdo, x, y, screen);
+        usleep(speedMS*1000);
+    }
+}
+
 void get_coords(int *x, int *y, int *screen) {
     xdo_get_mouse_location(xdo, x, y, screen);
 }
 
-void hold(int pressTime, int button) {
-    xdo_mouse_down(xdo, CURRENTWINDOW, button);
-    usleep(pressTime*1000);
-    xdo_mouse_up(xdo, CURRENTWINDOW, button);
+void hold(int holdTime, int button) {
+    if (access("/tmp/presslog", F_OK) != 0) {
+        xdo_mouse_down(xdo, CURRENTWINDOW, button);
+        usleep(holdTime*1000);
+        xdo_mouse_up(xdo, CURRENTWINDOW, button);
+    } else {
+        float dholdTime;
+        get_hold_time(&dholdTime);
+
+        xdo_mouse_down(xdo, CURRENTWINDOW, button);
+        usleep(dholdTime*1000);
+        xdo_mouse_up(xdo, CURRENTWINDOW, button);
+    }
 }
 
-void drag(float ix, float iy, float tx, float ty, int speedMS, int screen, int button) {
+void drag(float ix, float iy, float tx, float ty, int speedMS, int button, int screen) {
     float frac, loop, loops, x, y, posdx, posdy;
     float dx = tx - ix, dy = ty - iy;
     if (dx < 0 || dy < 0) {
@@ -97,14 +131,46 @@ void drag(float ix, float iy, float tx, float ty, int speedMS, int screen, int b
 }
 
 void get_pixel_color(char color[]) {
-    FILE *cmd = popen("eval $(xdotool getmouselocation --shell) ; xwd -root -silent | convert xwd:- -depth 8 -crop \"1x1+$X+$Y\" txt:- | grep -om1 '#\\w\\+'", "r");
+    FILE *cmd = popen("getpixelcolor", "r");
     fgets(color, 256, cmd);
     fclose(cmd);
 }
 
-int print_help(void) {
-    puts("usage: mouse [click {button}] [move {x,y,screen}] [move-rel {x,y}] [get {x,y,xy}] [getpixelcolor]");
-    return 0;
+int get_hold_time(float *holdTime) {
+    if (access("/tmp/presslog", F_OK) != 0) {
+        return 1;
+    } else {
+        char buf, repeatRate[24], repeatDelay[24];
+        float logLines;
+
+        FILE *file = fopen("/tmp/presslog", "r");
+        while ((buf = getc(file)) != EOF)
+            logLines = (buf == '\n') ? logLines + 1 : logLines;
+        fclose(file);
+        remove("/tmp/presslog");
+
+        FILE *frepeatDelay = popen("xset -q | awk NR==9 | awk '{print $4}'", "r");
+        fgets(repeatDelay, 24, frepeatDelay);
+        fclose(frepeatDelay);
+
+        FILE *frepeatRate = popen("xset -q | awk NR==9 | awk '{print $7}'", "r");
+        fgets(repeatRate, 24, frepeatRate);
+        fclose(frepeatRate);
+
+        *holdTime = logLines / atof(repeatRate) * 1000 + atof(repeatDelay);
+        return 0;
+    }
+}
+
+void print_help(void) {
+    puts("mouse [click {button}]");
+    puts("      [clickc {x,y,button,screen}]");
+    puts("      [clickcr {x,y,button,screen}]");
+    puts("      [move {x,y,screen}]");
+    puts("      [move-rel {x,y}]");
+    puts("      [hold {holdtime,button}]");
+    puts("      [drag {ix,iy,tx,ty,speedMS,button,screen}]");
+    puts("      [get {x,y,xy,holdtime,pixelcolor}]");
 }
 
 
@@ -120,7 +186,7 @@ int main(int argc, char **argv) {
 
     if (strcmp(action, "click") == 0)
     {
-        if (argc != 2) {
+        if (argc < 2) {
             print_help();
             return 1;
         }
@@ -130,7 +196,7 @@ int main(int argc, char **argv) {
 
     else if (strcmp(action, "clickc") == 0 || strcmp(action, "clickcr") == 0)
     {
-        if (argc != 4) {
+        if (argc < 4) {
             print_help();
             return 1;
         }
@@ -139,20 +205,16 @@ int main(int argc, char **argv) {
         int button = (argc > 4) ? atoi(argv[4]) : 1;
         int screen = (argc > 5) ? atoi(argv[5]) : 0;
         if (strcmp(action, "clickcr") == 0) {
-            click_cords_return(x, y, screen, button);
+            click_cords_return(x, y, button, screen);
         } else {
-            click_cords(x, y, screen, button);
+            click_cords(x, y, button, screen);
         }
     }
 
     else if (strcmp(action, "hold") == 0) {
-        if (argc != 3) {
-            print_help();
-            return 1;
-        }
-        int pressTime = atoi(argv[2]);
-        int button = (argc > 4) ? atoi(argv[4]) : 1;
-        hold(pressTime, button);
+        int holdTime = (argc > 2) ? atoi(argv[2]) : 0;
+        int button = (argc > 3) ? atoi(argv[3]) : 1;
+        hold(holdTime, button);
     }
 
     else if (strcmp(action, "drag") == 0) {
@@ -167,7 +229,7 @@ int main(int argc, char **argv) {
         int speedMS = (argc > 6) ? atoi(argv[6]) : 1;
         int button = (argc > 7) ? atoi(argv[7]) : 1;
         int screen = (argc > 8) ? atoi(argv[8]) : 0;
-        drag(initalX, initalY, targetX, targetY, speedMS, screen, button);
+        drag(initalX, initalY, targetX, targetY, speedMS, button, screen);
     }
 
     else if (strcmp(action, "move") == 0)
@@ -193,6 +255,20 @@ int main(int argc, char **argv) {
         move_rel(x, y);
     }
 
+    else if (strcmp(action, "move-smooth") == 0)
+    {
+        if (argc < 3) {
+            print_help();
+            return 1;
+        }
+
+        int x = atoi(argv[2]);
+        int y = atoi(argv[3]);
+        int speedMS = (argc > 4) ? atoi(argv[4]) : 1;
+        int screen = (argc > 5) ? atoi(argv[5]) : 0;
+        move_smooth(x, y, speedMS, screen);
+    }
+
     else if (strcmp(action, "get") == 0)
     {
         if (argc != 3) {
@@ -207,16 +283,26 @@ int main(int argc, char **argv) {
             printf("%d", x);
         } else if (strcmp(argv[2], "y") == 0) {
             printf("%d", y);
-        } else {
+        } else if (strcmp(argv[2], "xy") == 0) {
             printf("%d %d", x, y);
+        } else if (strcmp(argv[2], "holdtime") == 0) {
+            float holdTime;
+            int success;
+            success = get_hold_time(&holdTime);
+            if (success != 0) {
+                puts("'/tmp/presslog' does not exist.");
+                return 1;
+            } else {
+                printf("%.0f", holdTime);
+            }
+        } else if (strcmp(argv[2], "pixelcolor") == 0) {
+            char color[50];
+            get_pixel_color(color);
+            printf("%s", color);
+        } else {
+            print_help();
+            return 1;
         }
-    }
-
-    else if (strcmp(action, "getpixelcolor") == 0)
-    {
-        char color[50];
-        get_pixel_color(color);
-        printf("%s", color);
     }
 
     else {
